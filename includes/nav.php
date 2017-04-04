82facbad9d25b3dfa5db0a51f0cd743fe79c53fa
DonNinja <yngvileo@gmail.com>
2017-04-04 11:47:32 +0000

    Smá lögun á php hluta

--- includes/nav.php
+++ includes/nav.php
@@ -2,8 +2,8 @@
 		<ul>
 					<a href=""><li class="v1">FORSIÐA</li></a>
 					<a href=""><li class="v2">VERSLUN</li></a>
-					<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {echo '<a href=""><li class="v3">ACCOUNT INFORMATION & KARFA</li></a>'}; else {echo '<a href=""><li class="v3">SIGN INN</li></a>';} ?>
+					<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {echo '<a href=""><li class="v3">ACCOUNT INFORMATION & KARFA</li></a>';} else {echo '<a href=""><li class="v3">SIGN INN</li></a>';} ?>
 					<a href=""><li class="v4">UPPLÝSINGAR</li></a>
-					<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {echo '<a href=""><li class="v5">SKRÁ ÚT</li></a>'};?>
+					<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {echo '<a href=""><li class="v5">SKRÁ ÚT</li></a>';}?>
 				</ul>
 	</nav>
