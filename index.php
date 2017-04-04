0ef8710700a74632cae879cfdd15f0400ee4a585
DonNinja <yngvileo@gmail.com>
2017-04-04 11:28:40 +0000

    Gerði includes folder og file fyrir footer & nav

--- index.php
+++ index.php
@@ -4,20 +4,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="hopaverkefni">
-	<title>blómabúðin okkar</title>
+	<title>Blómabúðin okkar</title>
 	<link rel="stylesheet" type="text/css" href="layout.css">
 </head>
 <body>
-	<nav>
-		<ul>
-					<a href=""><li class="v1">FORSIÐA</li></a>
-					<a href=""><li class="v2">VERSLUN</li></a>
-					<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {echo '<a href=""><li class="v3">ACCOUNT INFORMATION</li></a>'}; else {echo '<a href=""><li class="v3">SIGN INN</li></a>'} ?>
-					<a href=""><li class="v4">UPPLÝSINGAR</li></a>
-				</ul>
-	</nav>
+	<?php require  './includes/nav.php'; ?>
 	<div class=""></div>
-
 
 	<?php require './includes/footer.php'; ?>
 </body>
