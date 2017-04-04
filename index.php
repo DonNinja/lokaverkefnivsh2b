8b63a026125815728fc085a9bffd5fa8bf19e9a0
hákon klaus <hkon klaus>
2017-04-04 17:07:43 +0000

    fylla i index simataerd

--- index.php
+++ index.php
@@ -7,11 +7,17 @@
 	<title>Bl&oacute;mab&uacute;&eth;in okkar</title>
 	<link rel="icon" href="http://www.gardheimar.is/img/logo.png">
 	<link rel="stylesheet" type="text/css" href="layout.css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Indie+Flower" />
 </head>
 <body>
 
 	<?php require  './includes/nav.php'; ?>
-	<div class=""></div>
+	<div class="kynna">
+		<br><h1>velkominn a bloma vef verslun yngva og klaus</h1><br>
+		<h3>a thessari sidu getur thu ablad upplysingar</h3><br>
+		<img src="img/indexmid.jpg">
+		<h3>um blomin okkar eda og keypt blom eda og frae theira</h3><br>
+	</div>
 
 	<?php require './includes/footer.php'; ?>
 </body>
