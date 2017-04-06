8da5f6175db25d81780fe8bd7e9ec7989b5c7ff7
hákon klaus <hkon klaus>
2017-04-06 15:08:51 +0000

    uppfærsla

--- account.php
+++ account.php
@@ -8,17 +8,8 @@
 	<link rel="stylesheet" type="text/css" href="layout.css">
 </head>
 <body>
-<nav>
-	<ul>
-				<a href=""><li class="v1">FORSIÐA</li></a>
-				<a href=""><li class="v2">VERSLUN</li></a>
-				<a href=""><li class="v3">SIGN INN</li></a><!-- PHP if in session þá account info -->
-				<a href=""><li class="v4">UPPLÝSINGAR</li></a>
-			</ul>
-</nav>
-<div class=""></div>
+<?php require  './includes/nav.php'; ?>
 
-<footer><h5>tæknemi® and all related logos and other elements are trademarks of tæknemi hf.
-©2016 tæknemi hf. All rights reserved. <a href=""> hafiðsamband</a></h5></footer>
+<?php require './includes/footer.php'; ?>
 </body>
 </html>
